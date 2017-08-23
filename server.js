92dd07f81103d4bb19031a17328356e813b969eb
sreeramkeerthan27 <sreekeerthan.1998@gmail.com>
2017-08-23 11:09:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-one',function(req,res){
+    res.send("Article-One requseted an will be Served");
+})
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
